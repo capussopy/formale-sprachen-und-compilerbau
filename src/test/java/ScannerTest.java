b8984b90bd2d49a4b01e18a466c37ec36e286de2
jan <jan.capasso@students.ffhs.ch>
2021-03-01 21:21:19 +0100

    create test grammer

--- src/test/java/ScannerTest.java
+++ src/test/java/ScannerTest.java
@@ -1,73 +1,59 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.*;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class ScannerTest {
 
-    //    private ByteArrayOutputStream outputStream;
-//
-//    @Before
-//    public void setUp() {
-//        outputStream = new ByteArrayOutputStream();
-//        System.setOut(new PrintStream(outputStream));
-//    }
-//
-//    @After
-//    public void tearDown() throws Exception {
-//        outputStream.close();
-//    }
-//
-//
+        private ByteArrayOutputStream outputStream;
+
+    @Before
+    public void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        outputStream.close();
+    }
+
+
     @Test
     public void output() throws Exception {
-        File inputFile = openFile("src/test/data/sample.in");
-//        assertThat(inputFile.isFile()).isTrue();
-//
+        File inputFile = openFile("src/test/resources/sample.in");
+
         String[] argv = new String[] {inputFile.getPath()};
-//
-        //Subst.main(argv);
-//
-//        // test actual is expected
-//        File expected = openFile("src/test/data/sample.expected");
-//        assertThat(expected.isFile()).isTrue();
-//
-//        BufferedReader actualContent = readOutputStream();
-//        BufferedReader expectedContent = new BufferedReader(new FileReader(expected));
-//
-//        for (int lineNumber = 1; lineNumber != -1; lineNumber++) {
-//            String expectedLine = expectedContent.readLine();
-//            String actualLine = actualContent.readLine();
-//            assertWithMessage("Line " + lineNumber).that(actualLine).isEqualTo(expectedLine);
-//            if (expectedLine == null) lineNumber = -2; // EOF
-//        }
-    }
-    //
-//    private BufferedReader readOutputStream() {
-//        byte[] rawOutput = outputStream.toByteArray();
-//        return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(rawOutput)));
-//    }
-//
-//    /**
-//     * Opens the given file.
-//     *
-//     * <p>This method also works around a build difficulty:
-//     *
-//     * <ul>
-//     *   <li>Maven uses the directory that contains {@code pom.xml} as a working directory, i.e.
-//     *       {@code examples/simple}
-//     *   <li>ant uses the directory that contains {@code build.xml} as a working directory, i.e.
-//     *       {@code examples/simple}
-//     *   <li>bazel uses the directory that contains {@code WORKSPACE} as a working directory, i.e.
-//     *       {@code __main__} in <em>runfiles</em>.
-//     * </ul>
-//     */
-    private File openFile(String pathName) throws IOException {
-        String path = pathName;
-        File pwd = new File(".").getCanonicalFile();
-        assertThat(pwd.isDirectory()).isTrue();
-        if (Objects.equals(pwd.getName(), "__main__")) {
-            path = "jflex/examples/simple/" + path;
+        Subst.main(argv);
+
+        // test actual is expected
+        File expected = openFile("src/test/resources/sample.expected");
+
+
+        BufferedReader actualContent = readOutputStream();
+        BufferedReader expectedContent = new BufferedReader(new FileReader(expected));
+
+        for (int lineNumber = 1; lineNumber != -1; lineNumber++) {
+            String expectedLine = expectedContent.readLine();
+            String actualLine = actualContent.readLine();
+            assertThat(actualLine).isEqualTo(expectedLine);
+           // assertWithMessage("Line " + lineNumber).that(actualLine).isEqualTo(expectedLine);
+            if (expectedLine == null) lineNumber = -2; // EOF
         }
+    }
+
+    private BufferedReader readOutputStream() {
+        byte[] rawOutput = outputStream.toByteArray();
+        return new BufferedReader(new InputStreamReader(new ByteArrayInputStream(rawOutput)));
+    }
+
+
+    private File openFile(String path) throws IOException {
         File file = new File(path);
         if (!file.isFile()) {
             throw new FileNotFoundException(path);
